f4b6f8c0259467cfe8b52d70312cc09250cb7aa2
James Keicher <jamesdkeicher@gmail.com>
2022-04-04 10:17:26 -0700

    second code challenge complete.

--- codechallenge.js
+++ codechallenge.js
@@ -4,6 +4,17 @@ const titleCase = (wordsArray) => {
     .map((word) => word[0].toUpperCase() + word.slice(1))
     .join(' ');
   console.log(mungedWords);
+  return mungedWords;
 };
 
 titleCase('this is a sentence');
+
+const reverseWords = (sentence) => {
+  const reversed = sentence
+    .split(' ')
+    .map((word) => word.split('').reverse().join(''));
+  console.log(reversed.join(' '));
+  return reversed.join(' ');
+};
+
+reverseWords('This is another sentence');
