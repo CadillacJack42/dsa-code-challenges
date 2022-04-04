412f80397dce1d863038587914addf114dbcf574
James Keicher <jamesdkeicher@gmail.com>
2022-04-04 11:07:36 -0700

    third and fourth code challenges complete

--- codechallenge.js
+++ codechallenge.js
@@ -18,3 +18,28 @@ const reverseWords = (sentence) => {
 };
 
 reverseWords('This is another sentence');
+
+const oddishEvenish = (number) => {
+  const check = number.toString().split('');
+  const checked = check.reduce((acc, curr) => acc + Number(curr), 0);
+  const isEven = checked % 2 === 0 ? 'Evenish' : 'Oddish';
+  console.log(isEven);
+  return isEven;
+};
+
+oddishEvenish(141);
+oddishEvenish(142);
+
+const at = (arr, index) => {
+  const verifyIndex = index > -1 ? true : false;
+  const verifyIndexExists = arr.length > index ? true : false;
+
+  const indexed = verifyIndex && verifyIndexExists && arr[index];
+  const negativeIndexed = arr[arr.length + index];
+  const returnValue = verifyIndex ? indexed : negativeIndexed;
+  console.log(returnValue);
+  return returnValue;
+};
+
+at(['1', 2, 'four', 9, 'eleven'], 3);
+at(['1', 2, 'four', 9, 'eleven'], -3);
